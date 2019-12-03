330bbeccd9c3d31341ce311f568fea2f8a3d056c
nmerk <nadyamerk@gmail.com>
2019-12-03 17:46:46 +0300

    hardcoded players added

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,76 @@
+let playersData = [
+    {   'name': 'Алексей Климкин',
+        'section': 'Секция 1',
+        'position': 'защитник слева',
+        'number': 1,
+    },
+    {   'name': 'Павел Башаринов',
+        'section': 'Секция 1',
+        'position': 'защитник справа',
+        'number': 2,
+    },
+    {   'name': 'Андрей Гардт',
+        'section': 'Секция 1',
+        'position': 'защитник слева',
+        'number': 3,
+    },
+    {   'name': 'Александр Горячев',
+        'section': 'Секция 2',
+        'position': 'защитник слева',
+        'number': 4,
+    },
+    {   'name': 'Алексей Захаров',
+        'section': 'Секция 3',
+        'position': 'защитник слева',
+        'number': 5,
+    },
+];
+
+let parentsBlock = document.getElementsByClassName('players')[0];
+playersData.forEach(el => {
+    let firstInlineEl = document.createElement('div');
+    firstInlineEl.classList.add('inline');
+
+    let nameEl = document.createElement('div');
+    nameEl.classList.add('player__name');
+    nameEl.innerHTML = el.name;
+
+    firstInlineEl.appendChild(nameEl);
+
+    if (el.section) {
+        let sectionEl = document.createElement('div');
+        sectionEl.classList.add('player__section');
+        sectionEl.innerHTML = el.section;
+
+        firstInlineEl.appendChild(sectionEl);
+    }
+
+    if (el.position) {
+        let positionEl = document.createElement('div');
+        positionEl.classList.add('player__position');
+        positionEl.innerHTML = el.position;
+
+        firstInlineEl.appendChild(positionEl);
+    }
+
+    let secondInlineEl = document.createElement('div');
+    secondInlineEl.classList.add('inline');
+
+    let numberEl = document.createElement('div');
+    numberEl.classList.add('player__number');
+    numberEl.innerHTML = el.number;
+
+    secondInlineEl.appendChild(numberEl);
+
+    let playerEl = document.createElement('div');
+    playerEl.classList.add('player');
+
+    playerEl.appendChild(firstInlineEl);
+    playerEl.appendChild(secondInlineEl);
+
+    parentsBlock.appendChild(playerEl);
+});
+
 let tabButtons = document.getElementsByClassName('tab');
 
 let playersTab = {
