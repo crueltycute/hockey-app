1dee43d9e739e09aa6095e1afc3dd7ebe7460607
nmerk <nadyamerk@gmail.com>
2019-12-03 15:09:18 +0300

    tabs done + eventlisteners

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,61 @@
+let tabButtons = document.getElementsByClassName('tab');
+
+let playersTab = {
+    "button": tabButtons[0],
+    "section": document.getElementById('tab-players'),
+    "visible": false,
+    "updateVisibility": function () {
+        this.visible = !this.visible;
+
+        this.visible ? this.button.classList.add('tab__focused') : this.button.classList.remove('tab__focused');
+    },
+    "hide": function () {
+        if (this.visible) {
+            this.section.classList.add('hidden');
+            this.updateVisibility();
+        }
+    },
+    "show": function () {
+        if (!this.visible) {
+            this.section.classList.remove('hidden');
+            this.updateVisibility();
+        }
+    }
+};
+
+let unitsTab = {
+    "button": tabButtons[1],
+    "section": document.getElementById('tab-units'),
+    "visible": true,
+    "updateVisibility": function () {
+        this.visible = !this.visible;
+
+        this.visible ? this.button.classList.add('tab__focused') : this.button.classList.remove('tab__focused');
+    },
+    "hide": function () {
+        if (this.visible) {
+            this.section.classList.add('hidden');
+            this.updateVisibility();
+        }
+    },
+    "show": function () {
+        if (!this.visible) {
+            this.section.classList.remove('hidden');
+            this.updateVisibility();
+        }
+    }
+};
+
+playersTab.button.addEventListener('click', () => {
+    unitsTab.hide();
+    playersTab.show();
+});
+
+unitsTab.button.addEventListener('click', () => {
+    playersTab.hide();
+    unitsTab.show();
+});
+
 let modal = document.getElementsByClassName('modal-section')[0];
 let modalVisible = false;
 
